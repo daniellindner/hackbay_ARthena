9492fe9d28a7050bd90c939d831ba9a515c399fa
Joel Jaeschke <huhnmonster@gmail.com>
2019-06-04 18:11:52 +0200

    Add server with net sockets

--- backend_server/src/graph/graph.js
+++
@@ -1,3 +0,0 @@
-var Graph = require('graph.js/dist/graph.full.js');
-var map = new Graph(["test1", { data: "hello" }], ["test2", { data: "hello2" }], ["test3", { data: "hello" }], [["test1", "test2"], { distance: 123 }]);
-console.log(map.toJSON());
--- backend_server/src/server/server.ts
+++ backend_server/src/server/server.ts
@@ -1,35 +1,24 @@
-const express = require("express");
-import * as http from 'http';
-import * as WebSocket from 'ws';
+const net = require('net');
+const server = net.createServer((c:any) => {
+  // 'connection' listener
+  console.log('client connected');
+  c.on('end', () => {
+    console.log('client disconnected');
+  });
+  c.write('hello\r\n');
+  setInterval(() => {
+    c.write(`${JSON.stringify({this: "is a test"})}\n`)
+  }, 2000)
 
-const app = express();
-
-//initialize a simple http server
-const server = http.createServer(app);
-
-const port = 9000;
-
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws: WebSocket) => {
-
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
-    setInterval(() => {
-        ws.send(JSON.stringify({hello: "this is a test"}));
-    }, 2000);
+  c.on("data", (data:any) => {
+      console.log(data.toString())
+  })
 });
 
-//start our server
-server.listen(port, () => {
-    console.log(`Server started on port ${port} :)`);
+server.on('error', (err:any) => {
+  console.log(err);
+});
+
+server.listen(9000, () => {
+  console.log('server bound');
 });
