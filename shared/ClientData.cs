e98558a8683249e9102c519b3e815588785f3aca
chris_g <chr33z@gmail.com>
2019-06-04 22:30:42 +0200

    client data

---
+++ shared/ClientData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+public class ClientData
+{
+    public UserData User;
+
+    public MachineData Machines;
+
+    public WaypointData Waypoints;
+
+    public class UserData
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+    }
+
+    public class MachineData
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public int id { get; set; }
+
+        public List<int> waypoint_ids;
+    } 
+
+    public class WaypointData
+    {
+        public int id { get; set; }
+
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public List<int> children { get; set; }
+
+        public bool is_exit { get; set; }
+        public bool is_machine { get; set; }
+    }
+}
